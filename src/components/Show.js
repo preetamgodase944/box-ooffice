cdce533bd84f3015ea8942878ae1d9361ff03b53
preetamgodase944 <preetamgodase944@gmail.com>
2021-09-22 21:06:16 +0530

    added loading and error message to the show page

--- src/components/Show.js
+++ src/components/Show.js
@@ -5,16 +5,39 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id }=useParams();
   const [ show, setShow]=useState(null);
-  
+  const [isLoading, setIsLoading]=useState(true);
+  const [error,setError]=useState(null);
   
   useEffect(()=>{
+
+    //  to prevent doing all this if the component is unmounted
+    let isMounted=true
     apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    })
+      
+      
+        if(isMounted){
+          setShow(results);
+          setIsLoading(false);
+        }
+       
+      
+    }).catch(err=>{
+      if(isMounted){
+
+        setError(err.message);
+        setIsLoading(false);
+      }
+    });
+    return ()=>{isMounted=false}
   }
   ,[id]);
 
-  console.log('Show',show)
+  if(isLoading){
+    return <div>Give us a minute...</div>
+  }
+  if(error){
+    return <div>Error occured: {error}</div>
+  }
   return (
     <div>
       This show page

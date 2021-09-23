e0a7a7f24e3fa55c423d65d748d2a4c675887ff4
preetamgodase944 <preetamgodase944@gmail.com>
2021-09-23 11:14:42 +0530

    replaced useState with useReducer in the Show page

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,12 +1,30 @@
-import React,{ useEffect, useState } from 'react';
+import React,{ useEffect, useReducer } from 'react';
 import { useParams} from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+
+const reducer=(prevState, action)=>{
+  switch(action.type){
+    case 'FETCH_SUCCESS':
+      return {show: action.show, isLoading: false, error: null}
+    case 'FETCH_FAILED':
+      return {isLoading: false, error: action.error}
+    default: 
+    return prevState ;
+  }
+}
+
+
+const initialState={
+  show:null,
+  isLoading:true,
+  error:null,
+};
 const Show = () => {
   const { id }=useParams();
-  const [ show, setShow]=useState(null);
-  const [isLoading, setIsLoading]=useState(true);
-  const [error,setError]=useState(null);
+
+  const [ {show, isLoading, error}, dispatch ]=useReducer( reducer, initialState )
+ 
   
   useEffect(()=>{
 
@@ -16,16 +34,13 @@ const Show = () => {
       
       
         if(isMounted){
-          setShow(results);
-          setIsLoading(false);
+          dispatch({type:'FETCH_SUCCESS', show:results})
         }
        
       
     }).catch(err=>{
       if(isMounted){
-
-        setError(err.message);
-        setIsLoading(false);
+        dispatch({type:'FETCH_FAILED', error:err.message})
       }
     });
     return ()=>{isMounted=false}
@@ -34,10 +49,11 @@ const Show = () => {
 
   if(isLoading){
     return <div>Give us a minute...</div>
-  }
+  };
   if(error){
     return <div>Error occured: {error}</div>
-  }
+  };
+
   return (
     <div>
       This show page
